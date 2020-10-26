0c0a53afce7e28c5688b15d838549cbe5b694381
Dementev Slava <kursant344@gmail.com>
2020-10-26 16:57:36 +0300

    Initial commit

---
+++ app/src/main/java/com/dementev/languageswitchingpage/MainActivity.java
@@ -0,0 +1,48 @@
+package com.dementev.languageswitchingpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private Spinner spinner;
+    private Button okBtn;
+    private Locale locale;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init(){
+        spinner = findViewById(R.id.spinner);
+        okBtn = findViewById(R.id.okBtn);
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (spinner.getSelectedItem().equals("Русский")){
+                    locale = new Locale("ru");
+                } else locale = new Locale("en");
+                Configuration configuration = new Configuration();
+                configuration.setLocale(locale);
+                getResources().updateConfiguration(configuration, getBaseContext().getResources().getDisplayMetrics());
+                recreate();
+            }
+        });
+
+    }
+
+
+}
